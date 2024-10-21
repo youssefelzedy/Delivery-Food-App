7da4e4335243fe5f43a1d8a62806e10df69a9529
Youssef Ahmed Hassan Elzedy <39376520+youssefelzedy@users.noreply.github.com>
2024-10-21 16:42:00 +0300

    Add initial server setup for Back-End

---
+++ Back-End/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+// const userRouter = require("./routes/userRoutes");
+// const apartmentRoutes = require("./routes/apartmentRoutes");
+// const reviewRouter = require("./routes/reviewRoutes");
+
+const app = express();
+
+// Development logging
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
+
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+
+app.get("/test", (req, res) => {
+  res.json("Hello World");
+});
+
+// app.use("/api/v1/apartments", apartmentRoutes);
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/reviews", reviewRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
