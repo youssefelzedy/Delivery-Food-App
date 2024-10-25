df8d0e50f558a9bec7351da3026fb2f934969660
Youssef Ahmed Hassan Elzedy <39376520+youssefelzedy@users.noreply.github.com>
2024-10-25 11:59:27 +0300

    Add restaurantController and restaurantRoutes for handling restaurant-related operations

---
+++ Back-End/controllers/restaurantController.js
@@ -0,0 +1,58 @@
+const Restaurant = require("./../models/restaurantModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+exports.getAllRestaurants = catchAsync(async (req, res, next) => {
+  const restaurants = await Restaurant.find();
+  res.status(200).json({
+    status: "success",
+    results: restaurants.length,
+    data: {
+      restaurants,
+    },
+  });
+});
+
+exports.createRestaurant = catchAsync(async (req, res, next) => {
+  const newRestaurant = await Restaurant.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: newRestaurant,
+    },
+  });
+});
+
+exports.getRestaurant = catchAsync(async (req, res, next) => {
+  const restaurant = await Restaurant.findById(req.params.id);
+  if (!restaurant) {
+    return next(new AppError("No restaurant found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant,
+    },
+  });
+});
+
+exports.updateRestaurant = catchAsync(async (req, res, next) => {
+  const restaurant = await Restaurant.findByIdAndUpdate(
+    req.params.id,
+    req.body
+  );
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant,
+    },
+  });
+});
+
+exports.deleteRestaurant = catchAsync(async (req, res, next) => {
+  await Restaurant.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
---
+++ Back-End/routes/restaurantRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const restaurantController = require("../controllers/restaurantController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(restaurantController.getAllRestaurants)
+  .post(restaurantController.createRestaurant);
+
+router
+  .route("/:id")
+  .get(restaurantController.getRestaurant)
+  .patch(restaurantController.updateRestaurant)
+  .delete(restaurantController.deleteRestaurant);
+
+module.exports = router;
