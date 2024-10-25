df521f3f58d94af15ac62ecbede0d32c91bcca9e
Youssef Ahmed Hassan Elzedy <39376520+youssefelzedy@users.noreply.github.com>
2024-10-25 11:59:14 +0300

    Add userController and userRoutes for handling user-related operations

---
+++ Back-End/controllers/userController.js
@@ -0,0 +1,58 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+   const users = await User.find();
+   res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+         users
+      }
+   });
+   }
+);
+
+exports.createUser = catchAsync(async (req, res, next) => {
+   const newUser = await User.create(req.body);
+   res.status(201).json({
+      status: 'success',
+      data: {
+         user: newUser
+      }
+   });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+   const user = await User.findById(req.params.id);
+   if (!user) {
+      return next(new AppError('No user found with that ID', 404));
+   }
+   res.status(200).json({
+      status: 'success',
+      data: {
+         user
+      }
+   });
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+   const user = await
+   User.findByIdAndUpdate(req.params);
+   res.status(200).json({
+      status: 'success',
+      data: {
+         user
+      }
+   });
+});
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+   await User.findByIdAndDelete(req.params.id);
+   res.status(204).json({
+      status: 'success',
+      data: null
+   });
+});
+
