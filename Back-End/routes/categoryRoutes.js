8184a0e81fa0da5b807669451abde2249bfd23ef
Youssef Ahmed Hassan Elzedy <39376520+youssefelzedy@users.noreply.github.com>
2024-11-18 15:16:53 +0200

    Refactor categoryRoutes.js to include CRUD operations for categories

---
+++ Back-End/routes/categoryRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const categoryController = require("../controllers/categoryController");
+
+const router = express.Router();
+
+router.route("/").get(categoryController.getAllCategories);
+
+router
+  .route("/:restaurantId")
+  .post(categoryController.createCategory)
+  .get(categoryController.getCategoriesForRestaurant);
+
+router
+  .route("/:restaurantId/:id")
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
+
+module.exports = router;
