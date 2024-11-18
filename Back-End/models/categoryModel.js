02a5e2adbdfc536ea364588e9718cc179ef747a5
Youssef Ahmed Hassan Elzedy <39376520+youssefelzedy@users.noreply.github.com>
2024-11-18 15:16:28 +0200

    Refactor categoryModel.js to include virtual field for foods

--- Back-End/models/categoryModel.js
+++ Back-End/models/categoryModel.js
@@ -8,7 +8,25 @@ const categorySchema = new mongoose.Schema({
   },
   name: { type: String, required: true },
   description: { type: String },
+  food: [
+    {
+      foodId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Food",
+        required: true,
+      },
+    },
+  ],
 });
+
+categorySchema.virtual("foods", {
+  ref: "Food", // Model to use
+  localField: "_id", // Field in the Category model
+  foreignField: "categoryId", // Field in the Food model
+});
+
+categorySchema.set("toObject", { virtuals: true }); // Ensure virtual fields are included when using toObject()
+categorySchema.set("toJSON", { virtuals: true }); // Ensure virtual fields are included when using toJSON()
 
 const Category = mongoose.model("Category", categorySchema);
 module.exports = Category;
